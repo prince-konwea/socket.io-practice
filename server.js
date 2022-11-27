11e2476509b71abb97c6fac699d633d2b68016a3
prince-konwea <kokodecontee123@gmail.com>
2022-11-27 21:23:42 +0100

    added html and css files to public folder

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+const cors = require("cors");
+
+app.use(cors({
+    origin: '*'
+}));
+
+
 const io = require("socket.io")(5000);
 
 io.on("connection", socket => {
     socket.emit("Chat-message", "Hello word")
+})
+
+const socket = io("http://localhost:5000");
+
+socket.on("chat-message", data =>{
+    console.log(data)
 })
